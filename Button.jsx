c319acedc4639da505edf20b07eaa3be07920cb4
kashish kumari <138995080+kashishku@users.noreply.github.com>
2024-11-24 19:27:21 +0530

    Add files via upload

---
+++ Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Button(props) {
+    const{text} = props
+  return (
+    <button className='px-8 py-4 mx-auto rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200'>
+        <p>{text}</p>
+    </button>
+    
+  )
+}
---
+++ Workout.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Workout(props) {
+  const {workout} = props
+  return (
+    <div>Workout</div>
+  )
+}
